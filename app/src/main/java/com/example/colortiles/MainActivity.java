4de7abbbec55e55cabf2f97ef587f4b55f564d52
Ivan Petrushin <ivan.kiel@gmail.com>
2019-11-19 15:56:04 +0800

    TilesView

--- app/src/main/java/com/example/colortiles/MainActivity.java
+++ app/src/main/java/com/example/colortiles/MainActivity.java
@@ -2,48 +2,26 @@ package com.example.colortiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int darkColor;
-    int brightColor;
-    View[][] tiles = new View[4][4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Resources r = getResources();
-        darkColor = r.getColor(R.color.dark);
-        brightColor = r.getColor(R.color.bright);
-        tiles[0][0] = findViewById(R.id.t00);
 
     }
-    public void changeColor(View v) {
-        ColorDrawable d = (ColorDrawable) v.getBackground();
-        if (d.getColor() == brightColor) {
-            v.setBackgroundColor(darkColor);
-        } else {
-            v.setBackgroundColor(brightColor);
-        }
-    }
-    public void onClick(View v) {
-        // получаем тэг на кнопке
-        String tag = v.getTag().toString();
-        int x = 1, y = 1; // координаты тайла и строки вида "00"
 
-        // изменить цвет на самом тайле и всех тайлах
-        // с таким же x и таким же y
-        changeColor(v);
-        for (int i = 0; i < 4; i++) {
-            changeColor(tiles[x][i]);
-            changeColor(tiles[i][y]);
-        }
-
-    }
 }
